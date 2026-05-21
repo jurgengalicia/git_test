d2bdf38f9935fdd53ca6c8bab75c132526699ef0
JurgenGalicia <jurgen.galicia@gmail.com>
2026-05-20 22:53:49 -0700

    guessnum exercise

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+let add7 = (x) => x+7;
+let multiply = (x, y) => x*y;
+let capitalize = function(stringToCap){
+    console.log(stringToCap.toLowerCase().charAt(0).toUpperCase());
+    console.log(`${stringToCap.charAt(0).toUpperCase()}${stringToCap.slice(1).toLowerCase()}`);
+};
 
-let a = 10;
-console.log(a) 
-a = 32;
-console.log(a) 
+let lastLetter = (x) => x.slice(-1);
 
-let b = 7 * a;
-console.log(b)
-
-const max = 57;
-const actual = max - 13;
-const percentage = actual / max;
-
-console.log("percentage",percentage);
+console.log(multiply(7,3));
+console.log(add7(7));
+capitalize("abcd");
+capitalize("ABCD");
+capitalize("aBcD");
+console.log(lastLetter("abcdefg"));
