26b8f07f0ecb196e6d1333492e1dc5b7e4efc056
JurgenGalicia <jurgen.galicia@gmail.com>
2026-05-30 20:31:46 -0700

    blastoff loop exercise

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-let add7 = (x) => x+7;
-let multiply = (x, y) => x*y;
-let capitalize = function(stringToCap){
-    console.log(stringToCap.toLowerCase().charAt(0).toUpperCase());
-    console.log(`${stringToCap.charAt(0).toUpperCase()}${stringToCap.slice(1).toLowerCase()}`);
-};
+const output = document.querySelector(".output");
+output.textContent = "";
 
-let lastLetter = (x) => x.slice(-1);
 
-console.log(multiply(7,3));
-console.log(add7(7));
-capitalize("abcd");
-capitalize("ABCD");
-capitalize("aBcD");
-console.log(lastLetter("abcdefg"));
+for(let i=10; i>=0; i--){
+  const para = document.createElement('p');
+  if(i == 10){
+    para.textContent += `begin countdown, ${i}`;
+  } else if(i == 0) {
+    para.textContent += "blastoff!";
+  } else {
+    para.textContent += `${i}`;
+  }
+  output.appendChild(para)
+}
+
